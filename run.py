1265bac3b7f1a5d57698a2119908b81651cddd52
23052015 <stani.ste13@gmail.com>
2023-11-08 20:49:01 +0000

    adjust code for displaying working hours and brekas, change minutes to hours

--- run.py
+++ run.py
@@ -1,27 +1,37 @@
 from flask import Flask, render_template, request
-from datetime import datetime
+from datetime import datetime, timedelta
+
 app = Flask(__name__)
 
-def calculate_hours(start, end):
-    # Convert start and end times to minutes
-    start_minutes = int(start.split(':')[0]) * 60 + int(start.split(':')[1])
-    end_minutes = int(end.split(':')[0]) * 60 + int(end.split(':')[1])
+def calculate_total_hours_and_break(workers, start, end):
+    start_time = datetime.strptime(start, "%H:%M")
+    end_time = datetime.strptime(end, "%H:%M")
 
-    # Calculate total working minutes
-    total_minutes = end_minutes - start_minutes
+    # Calculate total working time for one worker
+    total_time_per_worker = end_time - start_time
 
     # Apply break logic
-    if total_minutes > 240:  # 4 hours
-        total_minutes += 15
-    if total_minutes > 480:  # 8 hours
-        total_minutes += 30
+    if total_time_per_worker > timedelta(hours=4):  # 4 hours
+        total_time_per_worker += timedelta(minutes=15)
+    if total_time_per_worker > timedelta(hours=8):  # 8 hours
+        total_time_per_worker += timedelta(minutes=30)
 
-    # Convert total minutes back to hours and minutes
-    total_hours, remainder = divmod(total_minutes, 60)
+    # Calculate total working time for all workers
+    total_time = total_time_per_worker * workers
 
-    return f"{int(total_hours)}:{int(remainder):02d}"
+    # Calculate total break time for all workers
+    total_break_time = timedelta(minutes=0)
+    if total_time_per_worker > timedelta(hours=4):  # 4 hours per worker
+        total_break_time += timedelta(minutes=15) * (workers - 1)
+    if total_time_per_worker > timedelta(hours=8):  # 8 hours per worker
+        total_break_time += timedelta(minutes=30) * (workers - 1)
 
-    
+    return {
+        'workers': workers,
+        'total_hours': str(total_time),
+        'total_break': str(total_break_time)
+    }
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -29,10 +39,10 @@ def index():
         start_time = request.form['start_time']
         end_time = request.form['end_time']
 
-        # Calculate total hours for all workers
-        total_hours = calculate_hours(start_time, end_time) * workers
+        # Calculate total hours and break for all workers
+        result = calculate_total_hours_and_break(workers, start_time, end_time)
 
-        return render_template('index.html', total_hours=total_hours)
+        return render_template('index.html', result=result)
 
     return render_template('index.html')
 
