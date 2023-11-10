ac6e15b2327f1cbb15a1794ed97661dc2d086d2a
23052015 <stani.ste13@gmail.com>
2023-11-10 21:50:30 +0000

    convert days to hours and minutes

--- run.py
+++ run.py
@@ -3,6 +3,13 @@ from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
+
+def format_timedelta(td):
+    hours, remainder = divmod(td.seconds, 3600)
+    minutes, _ = divmod(remainder, 60)
+    return f"{hours:02}:{minutes:02}"
+
+
 def calculate_total_hours_and_break(workers, start, end):
     start_time = datetime.strptime(start, "%H:%M")
     end_time = datetime.strptime(end, "%H:%M")
@@ -17,21 +24,25 @@ def calculate_total_hours_and_break(workers, start, end):
     if total_time_per_worker >= timedelta(hours=8):
         break_time += timedelta(minutes=15)
 
-        
-
     # Calculate total working time for all workers
     total_time = total_time_per_worker * workers
 
     # Calculate total break time for all workers
     total_break_time = break_time * workers
 
+    # Convert total_time to hours and minutes
+    total_hours, remainder = divmod(int(total_time.total_seconds()), 3600)
+    total_minutes = remainder // 60
+    
+
+    ## Calculate total working hours for all workers
     total_working_time = total_time - total_break_time
 
     return {
         'workers': workers,
-        'total_hours': str(total_time),
-        'total_break': str(total_break_time),
-        'total_working_hours': str(total_working_time)
+        'total_hours': f"{total_hours:02}:{total_minutes:02}",
+        'total_break': format_timedelta(total_break_time),
+        'total_working_hours': str(total_working_time),
     }
 
 @app.route('/', methods=['GET', 'POST'])
