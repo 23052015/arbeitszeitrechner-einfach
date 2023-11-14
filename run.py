23993eac0077d694cf741720a6256dba06baa0bc
23052015 <stani.ste13@gmail.com>
2023-11-14 12:46:27 +0000

    change the display of total working hours to be in minutes and hours and round it to he nearest quarter hour

--- run.py
+++ run.py
@@ -10,9 +10,15 @@ def format_timedelta(td):
     return f"{hours:02}:{minutes:02}"
 
 
+def round_to_nearest_quarter_hour(dt):
+    minutes = dt.minute
+    rounded_minutes = 15 * round(minutes / 15)
+    return dt.replace(minute=rounded_minutes, second=0, microsecond=0)
+
+
 def calculate_total_hours_and_break(workers, start, end):
-    start_time = datetime.strptime(start, "%H:%M")
-    end_time = datetime.strptime(end, "%H:%M")
+    start_time = round_to_nearest_quarter_hour(datetime.strptime(start, "%H:%M"))
+    end_time = round_to_nearest_quarter_hour(datetime.strptime(end, "%H:%M"))
 
     # Calculate total working time for one worker
     total_time_per_worker = end_time - start_time
@@ -30,19 +36,24 @@ def calculate_total_hours_and_break(workers, start, end):
     # Calculate total break time for all workers
     total_break_time = break_time * workers
 
+    ## Calculate total working hours for all workers
+    total_working_time = total_time - total_break_time
+
     # Convert total_time to hours and minutes
     total_hours, remainder = divmod(int(total_time.total_seconds()), 3600)
     total_minutes = remainder // 60
-    
 
-    ## Calculate total working hours for all workers
-    total_working_time = total_time - total_break_time
+    total_hours_1, remainder_1 = divmod(int(total_working_time.total_seconds()), 3600)
+    total_minutes_1 = remainder_1 // 60
+
+
+
 
     return {
         'workers': workers,
         'total_hours': f"{total_hours:02}:{total_minutes:02}",
         'total_break': format_timedelta(total_break_time),
-        'total_working_hours': str(total_working_time),
+        'total_working_hours': f"{total_hours_1:02}:{total_minutes_1:02}",
     }
 
 @app.route('/', methods=['GET', 'POST'])
